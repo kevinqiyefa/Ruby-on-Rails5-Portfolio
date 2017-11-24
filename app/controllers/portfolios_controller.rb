5b009c4bca377e1d4e9ce532dde506e413012057
Kevin <kevinqiyf@hotmail.com>
2017-11-24 20:36:33 +0000

    implemented delete action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,5 +40,13 @@ class PortfoliosController < ApplicationController
         end
     end
     
+    def destroy
+      @portfolio_item = Portfolio.find(params[:id])
+      
+      @portfolio_item.destroy
+      respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully removed.' }
+      end
+    end
     
 end 
