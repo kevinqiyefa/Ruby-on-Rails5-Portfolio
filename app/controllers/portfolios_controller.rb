fafbb611b60fa89a5e37190d05d338d525e8fbdb
Kevin <kevinqiyf@hotmail.com>
2017-11-24 08:08:27 +0000

    Integrated Edit and Update actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,17 +8,33 @@ class PortfoliosController < ApplicationController
     end
     
     def create
-        @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
     
         respond_to do |format|
-          if @portfolio_items.save
+          if @portfolio_item.save
             format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully created.' }
             
           else
             format.html { render :new }
-            
           end
         end
     end 
+    
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+    
+    def update
+        @portfolio_item = Portfolio.find(params[:id])
+        respond_to do |format|
+          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+            format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+          else
+            format.html { render :edit }
+
+          end
+        end
+    end
+    
     
 end 
