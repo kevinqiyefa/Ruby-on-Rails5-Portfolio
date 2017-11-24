5d9c9b7283974dc59d1f9c64867383353f199dcd
Kevin <kevinqiyf@hotmail.com>
2017-11-24 20:13:28 +0000

    implemented show functionality for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,10 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.new
     end
     
+    def show
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+    
     def create
         @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
     
