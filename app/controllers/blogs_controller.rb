9ae963b7c8dd343b4b6d7513667c31e77ea58844
Kevin <kevinqiyf@hotmail.com>
2017-11-25 23:28:08 +0000

    updated custom action for updating status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -60,6 +60,16 @@ class BlogsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+      
+    redirect_to blogs_url, notice: 'Post status had been updated.'
+  end 
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
 
   get 'contact', to: 'pages#contact'
 
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   
   root to: 'pages#home'
   
