f170a50e26093cbe1df916d0c82ab173fda69820
Kevin <kevinqiyf@hotmail.com>
2017-11-27 00:43:42 +0000

    Implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,12 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
+    
+    def self.angular
+        where(subtitle: 'Angular')
+    end
+    
+    
+    # OR
+    
+    scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails')}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
 
 10.times do |blog|
     Blog.create!(
         title: "My Blog Post #{blog}",
-        body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. "
+        body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. ",
+        topic_id: Topic.last.id
     )
 end
 
@@ -26,10 +32,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
    Portfolio.create!(
        title: "Portfolio title: #{portfolio_item}",
-       subtitle: "My great service",
+       subtitle: "Ruby on Rails",
+       body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+       main_image: "http://via.placeholder.com/600x400",
+       thumb_image: "http://via.placeholder.com/350x200" 
+    )
+end
+
+1.times do |portfolio_item|
+   Portfolio.create!(
+       title: "Portfolio title: #{portfolio_item}",
+       subtitle: "Angular",
        body: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
        main_image: "http://via.placeholder.com/600x400",
        thumb_image: "http://via.placeholder.com/350x200" 
