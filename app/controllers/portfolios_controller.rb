115cd1496841ee503bc60433d16080f738c97ee5
Kevin <kevinqiyf@hotmail.com>
2017-12-17 08:35:19 +0000

    Refactored strong params for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -17,7 +17,7 @@ class PortfoliosController < ApplicationController
     end
     
     def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+        @portfolio_item = Portfolio.new(portfolio_params )
     
         respond_to do |format|
           if @portfolio_item.save
@@ -36,7 +36,7 @@ class PortfoliosController < ApplicationController
     def update
         @portfolio_item = Portfolio.find(params[:id])
         respond_to do |format|
-          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+          if @portfolio_item.update(portfolio_params)
             format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
           else
             format.html { render :edit }
@@ -52,6 +52,12 @@ class PortfoliosController < ApplicationController
       respond_to do |format|
         format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully removed.' }
       end
+    end
+    
+    private
+    
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]) 
     end
     
 end 
