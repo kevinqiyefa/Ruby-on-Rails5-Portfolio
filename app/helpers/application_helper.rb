a5b97760d7fb0387943047df967d33eb3d4632c3
Kevin <kevinqiyf@hotmail.com>
2018-01-19 04:40:23 +0000

    Implemented refactor for navigaition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-    def login_helper style=""
+    def login_helper style=" "
         if current_user.is_a?(GuestUser)
             (link_to "Register", new_user_registration_path, class: style) +
             " ".html_safe +
@@ -19,6 +19,53 @@ module ApplicationHelper
     
     def copyright_generator
        KevinViewTool::Renderer.copyright "Kevin Qi", "All rights reserved" 
+    end
+    
+    
+    def nav_items
+        [
+            {
+                url: root_path,
+                title: 'Home'
+            },
+            
+            {
+                url: about_path,
+                title: 'About'
+            },
+            
+            {
+                url: contact_path,
+                title: 'Contact'
+            },
+            
+            {
+                url: blogs_path,
+                title: 'Blog'
+            },
+            
+            {
+                url: portfolios_path,
+                title: 'Portfilo'
+            }
+            
+        ]
+    end
+    
+    
+    def nav_helper style, tag_type
+        nav_links = ''
+        nav_items.each do |item|
+            nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+        end
+        
+        nav_links.html_safe
+        
+    end
+    
+    
+    def active? path
+        'active' if current_page? path
     end
 
 end
