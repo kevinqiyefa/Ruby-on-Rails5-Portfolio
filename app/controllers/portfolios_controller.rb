0cf60b37297f5c56c80c73b115a91cb6c23c3f6b
Kevin <kevinqiyf@hotmail.com>
2018-01-23 06:50:20 +0000

    Integrated cocoon for portfolio items nested from component

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,7 +25,7 @@ class PortfoliosController < ApplicationController
     
     def new
         @portfolio_item = Portfolio.new
-        3.times { @portfolio_item.technologies.build  }
+        
     end
     
     def show 
@@ -70,7 +70,7 @@ class PortfoliosController < ApplicationController
     private
     
     def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, technologies_attributes: [:name]) 
+      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, technologies_attributes: [:id, :name, :_destroy]) 
     end
     
     def set_portfolio_item
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,8 @@
 class Portfolio < ApplicationRecord
     has_many :technologies
-    accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
+    accepts_nested_attributes_for :technologies, 
+                                    allow_destroy: true, 
+                                    reject_if: lambda { |attrs| attrs['name'].blank? }
              
     mount_uploader :thumb_image, PortfolioUploader
     mount_uploader :main_image, PortfolioUploader
