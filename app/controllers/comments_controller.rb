f2b57426849655705f2fc7564aa8e36eb51e0292
Kevin <kevinqiyf@hotmail.com>
2018-01-28 06:52:46 +0000

    Implemented action for comments and fixed acation cable bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,11 @@
 class CommentsController < ApplicationController
-    
-    def create
-        @comment = current_user.comments.build(comment_params)
-    end
-    
-    private
-    
-    def comment_params
-        params.require(:comment).permit(:content)
-    end
-    
+  def create
+    @comment = current_user.comments.build(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
