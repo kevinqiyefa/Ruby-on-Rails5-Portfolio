126b19452c243e0b25c67c105b35dc297228e1c8
Kevin <kevinqiyf@hotmail.com>
2018-02-03 22:09:57 +0000

    Updated gemfile and secret key base call

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module MyApp
     config.generators.system_tests = nil
     
     config.eager_load_paths << "#{Rails.root}/lib"
-    
+    config.secret_key_base = ENV['SECRET_KEY_BASE']
     
   end
 end
