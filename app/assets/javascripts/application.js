f65f4c1324c9f72efea92ca3e115e478847db685
Kevin <kevinqiyf@hotmail.com>
2018-02-05 05:08:21 +0000

    Fixed JS loading bug and Integrated Redis cloud to heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
@@ -21,3 +20,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
